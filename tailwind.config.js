6afc38b8d143b7ec71b8aed92df4d72746edc360
Hubert Bieszczad <brentlokk@gmail.com>
2022-11-24 11:42:34 +0100

    Initial App

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.ts",
+    "./components/**/*.ts",
+    "./bits/**/*.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
